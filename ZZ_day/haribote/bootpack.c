94514d0caf1603962c5b0abb40cff5e3f87835ff
杉森隆行 <sugimori@fc.jpn.org>
2020-05-06 16:13:53 +0900

    (harib02f)文字列の表示

--- ZZ_day/haribote/bootpack.c
+++ ZZ_day/haribote/bootpack.c
@@ -12,8 +12,7 @@ void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 void init_screen(unsigned char *vram, int xsize, int ysize);
 void putfont8(unsigned char *vram, int xsize, int x, int y, char color, char *font);
-
-extern char hankaku[4096];
+void putfonts8_asc(char *vram, int xsize, int x, int y, char color, unsigned char *s);
 
 /* カラーパレットの定義 */
 #define COL8_000000		0
@@ -43,29 +42,30 @@ struct BOOTINFO {
 void HariMain(void)
 {
 	struct BOOTINFO *binfo;
-	static char font_A[16] = {
-		0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
-		0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
-	};
 
 	init_palette();
-
 	binfo = (struct BOOTINFO *)0x0ff0;
 
 	init_screen(binfo->vram,binfo->scrnx,binfo->scrny);
 
-	putfont8(binfo->vram,binfo->scrnx, 10,20,COL8_FFFFFF,hankaku + 'B' * 16);
-	putfont8(binfo->vram, binfo->scrnx,  8, 8, COL8_FFFFFF, hankaku + 'A' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 16, 8, COL8_FFFFFF, hankaku + 'B' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 24, 8, COL8_FFFFFF, hankaku + 'C' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 40, 8, COL8_FFFFFF, hankaku + '1' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 48, 8, COL8_FFFFFF, hankaku + '2' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 56, 8, COL8_FFFFFF, hankaku + '3' * 16);
+	putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8, COL8_FFFFFF, "ABC 123");
+	putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS.");
+	putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS.");
 
 	for(;;) {
 		io_hlt();
 	}
+	return;
+}
 
+void putfonts8_asc(char *vram, int xsize, int x, int y, char color, unsigned char *s)
+{
+	extern char hankaku[4096];
+	for(;*s!=0x00;s++) {
+		putfont8(vram, xsize,x,y,color,hankaku + *s * 16);
+		x += 8;
+	}
+	return;
 }
 
 void putfont8(unsigned char *vram, int xsize, int x, int y, char color, char *font)
