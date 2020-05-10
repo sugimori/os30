3382d153f8582dd018c24fb0d52bbb660a2cf019
杉森隆行 <sugimori@fc.jpn.org>
2020-05-10 19:27:36 +0900

    bootpack.cを分割

---
+++ ZZ_day/haribote/bootpack.h
@@ -0,0 +1,62 @@
+/* naskfunc.nas */
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+
+/* asmhead.nas */
+struct BOOTINFO {
+	char cyls,leds,vmode,reserve;
+	short scrnx,scrny;
+	char *vram;
+};
+
+
+/* graphic.c */
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen(unsigned char *vram, int xsize, int ysize);
+void putfont8(unsigned char *vram, int xsize, int x, int y, char color, char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char color, unsigned char *s);
+int sprintf (char *str, char *fmt, ...);
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
+void init_mouse_cursor8(char *mouse, char bc) ;
+
+/* カラーパレットの定義 */
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
+
+/* dsctbl.c */
+struct SEGMENT_DESCRIPTOR {
+	short limit_low, base_low;    // 2 2
+	char base_mid, access_right;  // 1 1 
+	char limit_high, base_high;	  // 1 1
+};
+
+struct GATE_DESCRIPTOR {
+	short offset_low, selector;		// 2 2
+	char dw_count, access_right;	// 1 1
+	short offset_high;				// 2
+};
+
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR * sd, unsigned int limit, int base ,int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
