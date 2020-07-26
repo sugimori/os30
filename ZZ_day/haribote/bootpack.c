2e1ee6c02b6a0fd06b7c06e96497a026e52966b9
杉森隆行 <sugimori@fc.jpn.org>
2020-07-26 18:09:53 +0900

    メモリチェックで失敗する予定だったが、うまく行ってしまった

--- ZZ_day/haribote/bootpack.c
+++ ZZ_day/haribote/bootpack.c
@@ -1,6 +1,9 @@
 #include "bootpack.h"
 
 extern struct FIFO8 keyfifo,mousefifo;
+unsigned int memtest(unsigned int start, unsigned int end);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
+
 
 void HariMain(void)
 {
@@ -29,6 +32,11 @@ void HariMain(void)
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+	/* メモリチェック */
+	i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
+	sprintf(s,"memory %dMB", i);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 
 	enable_mouse(&mdec);
 
@@ -78,4 +86,64 @@ void HariMain(void)
 		}
 	}
 	return;
+}
+
+#define EFLAGS_AC_BIT		0x00040000
+#define CR0_CACHE_DISABLE	0x60000000
+
+unsigned int memtest(unsigned int start, unsigned int end)
+{
+	char flg486 = 0;
+	unsigned int eflg, cr0, i;
+
+	// 386か、486か
+	eflg = io_load_eflags();
+	eflg |= EFLAGS_AC_BIT; // AC-bit = 1
+	io_store_eflags(eflg);
+	eflg = io_load_eflags();
+	if((eflg & EFLAGS_AC_BIT) != 0) {
+		flg486 = 1;
+	}
+	eflg &= ~EFLAGS_AC_BIT; // AC-bit = 0
+	io_store_eflags(eflg); // AC 0で書き込み
+
+	if(flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 |= CR0_CACHE_DISABLE; // キャッシュ禁止
+		store_cr0(cr0);
+	}
+
+	i = memtest_sub(start, end);
+
+	if(flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 &= ~CR0_CACHE_DISABLE; // キャッシュ許可
+		store_cr0(cr0);
+	}
+
+	return i;
+
+}
+
+unsigned int memtest_sub(unsigned int start, unsigned int end)
+{
+	unsigned int i, *p, old, pat0 = 0xaa55aa55, pat1 = 0x55aa55aa;
+	for( i = start; i<= end; i += 4) {
+		p = (unsigned int *) i;
+		old = *p; 
+		*p = pat0;
+		*p ^= 0xffffffff;
+		if( *p != pat1) {
+not_memory:
+			*p = old;
+			break;
+		}
+
+		*p ^= 0xffffffff;
+		if(*p != pat0) {
+			goto not_memory;
+		}
+		*p = old;
+	}
+	return i;
 }
--- ZZ_day/haribote/naskfunc.nas
+++ ZZ_day/haribote/naskfunc.nas
@@ -17,6 +17,7 @@
 		GLOBAL	io_load_eflags,io_store_eflags
 		GLOBAL	load_gdtr, load_idtr
 		GLOBAL	asm_inthandler21, asm_inthandler27, asm_inthandler2c
+		GLOBAL	load_cr0, store_cr0
 		EXTERN	inthandler21, inthandler2c, inthandler27
 
 
@@ -146,3 +147,14 @@ asm_inthandler2c:
 		POP		DS
 		POP		ES
 		IRETD
+
+load_cr0:		; int load_cr0(void);
+	MOV		EAX,CR0
+	RET
+
+
+store_cr0:		; void store_cr0(int cr0);
+	MOV		EAX,[ESP+4]
+	MOV		CR0,EAX
+	RET
+	
