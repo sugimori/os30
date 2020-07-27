566ea96a7cbffb7c3df481ba4af2ad7ef5eb6051
杉森隆行 <sugimori@fc.jpn.org>
2020-07-27 17:28:15 +0900

    メモリ関連をmemory.cにリファクタリング

--- ZZ_day/haribote/bootpack.h
+++ ZZ_day/haribote/bootpack.h
@@ -128,3 +128,30 @@ struct MOUSE_DEC {
 void enable_mouse(struct MOUSE_DEC *mdec);
 int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 void inthandler2c(int *esp);
+
+
+/* memory.c */
+
+#define MEMMAN_ADDR		0x003c0000
+#define MEMMAN_FREES		4090
+#define EFLAGS_AC_BIT		0x00040000
+#define CR0_CACHE_DISABLE	0x60000000
+
+struct FREEINFO
+{
+	/* data */
+	unsigned int addr, size;
+};
+
+struct MEMMAN
+{
+	/* data */
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+unsigned int memtest(unsigned int start, unsigned int end);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
