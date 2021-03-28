3b08bbadb8978d5bf557f5117e464b74c1b73952
杉森隆行 <sugimori@fc.jpn.org>
2021-03-28 18:54:46 +0900

    (harib12f)スピード測定

--- ZZ_day/haribote/bootpack.c
+++ ZZ_day/haribote/bootpack.c
@@ -247,9 +247,9 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c)
 void task_b_main(struct SHEET *sht_back)
 {
 	struct FIFO32 fifo;
-	struct TIMER *timer_ts, *timer_put;
-	int i, fifobuf[128], count = 0;
-	char s[12];
+	struct TIMER *timer_ts, *timer_put, *timer_1s;
+	int i, fifobuf[128], count = 0, count0 = 0;
+	char s[20];
 
 	fifo32_init(&fifo, 128, fifobuf);
 	timer_ts = timer_alloc();
@@ -258,6 +258,9 @@ void task_b_main(struct SHEET *sht_back)
 	timer_put = timer_alloc();
 	timer_init(timer_put, &fifo, 1);
 	timer_settime(timer_put, 1);
+	timer_1s = timer_alloc();
+	timer_init(timer_1s, &fifo, 100);
+	timer_settime(timer_1s, 100);
 
 	for(;;) {
 		count++;
@@ -272,13 +275,18 @@ void task_b_main(struct SHEET *sht_back)
 			i = fifo32_get(&fifo);
 			io_sti();
 			// io_stihlt();
-			if(i==1) {
+			if( i == 1 ) {
 				sprintf(s, "%11d", count);
 				putfonts8_asc_sht(sht_back, 0, 180, COL8_FFFFFF, COL8_008484, s, 11);
 				timer_settime(timer_put, 1);
-			} else if ( i == 2) {
+			} else if ( i == 2 ) {
 				farjmp(0, 3 * 8);
 				timer_settime(timer_ts, 2);
+			} else if ( i == 100 ) {
+				sprintf(s, "SPEED:%11d", count - count0);
+				putfonts8_asc_sht(sht_back, 0, 200, COL8_FFFFFF, COL8_008484, s, 17);
+				count0 = count;
+				timer_settime(timer_1s, 100);
 			}
 		}
 	}
