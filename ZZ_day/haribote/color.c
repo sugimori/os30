9255cd667ae761feb95bc60afec075957a5756f3
杉森隆行 <sugimori@fc.jpn.org>
2021-06-27 15:39:43 +0900

    (harib22b) rとgを６段階

--- ZZ_day/haribote/color.c
+++ ZZ_day/haribote/color.c
@@ -28,10 +28,10 @@ void HariMain(void) {
   win = api_openwin(buf, 144, 164, -1, "color");
   for (y = 0; y < 128; y++) {
     for (x = 0; x < 128; x++) {
-      r = x * 2;
-      g = y * 2;
+      r = x;
+      g = y;
       b = 0;
-      buf[(x + 8) + (y + 28) * 144] = 16 + (r / 43) + (g / 43) * 6 + (b / 43) * 36;
+      buf[(x + 8) + (y + 28) * 144] = 16 + (r * 6 / 129) + (g * 6 / 129) * 6 + (b * 6 / 129) * 36;
     }
   }
   api_refreshwin(win, 8, 28, 136, 156);
