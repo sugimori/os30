71a5b039c0fb07f5e1c3bdc4eb95304185d3b6e7
杉森隆行 <sugimori@fc.jpn.org>
2021-07-23 15:32:31 +0900

    (harib24d)アプリのセグメントをローカルセグメントに

--- ZZ_day/haribote/console.c
+++ ZZ_day/haribote/console.c
@@ -377,12 +377,12 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline) {
       dathrb = *((int *)(p + 0x0014));
       q = (char *)memman_alloc_4k(memman, seqsiz);
       task->ds_base = (int)q;  //*((int *)0xfe8) = (int)q;
-      set_segmdesc(gdt + task->sel / 8 + 1000, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
-      set_segmdesc(gdt + task->sel / 8 + 2000, seqsiz - 1, (int)q, AR_DATA32_RW + 0x60);
+      set_segmdesc(task->ldt + 0, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
+      set_segmdesc(task->ldt + 1, seqsiz - 1, (int)q, AR_DATA32_RW + 0x60);
       for (i = 0; i < datsiz; i++) {
         q[esp + i] = p[dathrb + i];
       }
-      start_app(0x1b, task->sel + 1000 * 8, esp, task->sel + 2000 * 8, &(task->tss.esp0));
+      start_app(0x1b, 0 * 8 + 4, esp, 1 * 8 + 4, &(task->tss.esp0));  // +4でLDT内のセグメンという意味
       // 終了処理
       shtctl = (struct SHTCTL *)*((int *)0x0fe4);
       for (i = 0; i < MAX_SHEETS; i++) {
--- ZZ_day/haribote/crack7.nas
+++ ZZ_day/haribote/crack7.nas
@@ -2,13 +2,13 @@ BITS 32
     GLOBAL    HariMain
 
 HariMain:
-    MOV   AX, 1005*8
+    MOV   AX, 4
     MOV   DS,AX
     CMP   DWORD [DS:0x0004],'Hari'
     JNE   fin
 
     MOV   ECX,[DS:0x0000]     ; こあアプリのデータセグメントの大きをを読み取る
-    MOV   AX,2005*8
+    MOV   AX,12
     MOV   DS,AX
 
 crackloop:
